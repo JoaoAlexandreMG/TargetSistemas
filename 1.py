bb5e9b4456218733b38805a15a40293106e024e2
João Alexandre <j.sa@ufu.br>
2024-09-17 22:41:17 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,21 +3,16 @@ def pesquisar_elemento(target: int):
         target = int(target)
     except ValueError:
         raise Exception("O parâmetro passado para a função não é um número!")
-    pertence = False
     if target in (0, 1, 2):
-        pertence = True
+        return (f"{target} pertence a sequência")
     a = 1
     b = 2
     while b < target:
         a, b = b, a + b
-
-    if target == b:
-        pertence = True
-    return (
-        f"{target} pertence a sequência"
-        if pertence
-        else f"{target} não pertence a sequência!"
-    )
+    else:
+        if target == b:
+            return (f"{target} pertence a sequência")
+    return (f"{target} não pertence a sequência!")
 
 
 if __name__ == "__main__":
